ef73478ba7ace27f70f7698888f2878b68a3c5c6
libo199618 <859975004@qq.com>
2018-11-06 18:32:15 +0800

    add 日考题

---
+++ 20181106.php
@@ -0,0 +1,18 @@
+<?php
+//日考题
+function nu($n,$m) {
+    $number = 0;
+    for($i=$n;$i<=$m;$i++) {
+        $a = substr_count((string)$i,"1");
+        if($a!==false) {
+            $number = $number+$a;
+        }
+        //echo $i."\n";
+    }
+    echo $number;
+}
+nu(1,13);
+
+
+
+?>
